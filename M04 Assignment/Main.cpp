06b04c5d5ce2efc9bcbe096e0f0e3c6f33d150f5
ArtistMonkey83 <yolie@yolandareyes.com>
2020-02-17 22:16:13 -0800

    Module 4 Assignment

---
+++ M04 Assignment/Main.cpp
@@ -0,0 +1,21 @@
+#include "Matrices-3.h"
+#include "../std_lib_facilities"
+
+int main()
+{
+  try
+  {
+    Matrix alpha(){0,0};   //constructing a maxtrix and intializing it's elements to zero
+
+    return 0;
+  }
+  catch(...)
+  {
+
+
+    return 2;
+  }
+
+  keep_window_open();
+
+}
